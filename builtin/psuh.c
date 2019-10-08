cc61f4e17be4344924b28e79db0c516656e66ff2
kleinlicht <>
2019-10-08 09:33:06 +0200

    psuh: print the current branch
    Signed-off-by: kleinlicht <>

--- builtin/psuh.c
+++ builtin/psuh.c
@@ -1,11 +1,13 @@
 #include "builtin.h"
 #include "config.h"
+#include "wt-status.h"
 
 int cmd_psuh(int argc, const char **argv, const char *prefix)
 {
     
     int i;
     const char *cfg_name;
+    struct wt_status status;
     
     printf(_("Pedro sopla una hoja.\n"));
 
@@ -20,10 +22,15 @@ int cmd_psuh(int argc, const char **argv, const char *prefix)
            prefix ? "/" : "", prefix ? prefix : "");
     
     git_config(git_default_config, NULL);
+    
     if (git_config_get_string_const("user.name", &cfg_name) > 0)
         printf(_("No name is found in config\n"));
     else
         printf(_("Your name: %s\n"), cfg_name);
+    
+    wt_status_prepare(the_repository, &status);
+    git_config(git_default_config, &status);
+    printf(_("Your current branch: %s\n"), status.branch);
     
     return 0;
 }
