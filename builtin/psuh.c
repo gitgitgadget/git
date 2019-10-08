83a10fd5dba16173e7c763321833c0fd33e5c761
kleinlicht <>
2019-10-08 09:48:26 +0200

    psuh: display the top of origin/master
    Signed-off-by: kleinlicht <>

--- builtin/psuh.c
+++ builtin/psuh.c
@@ -1,6 +1,8 @@
 #include "builtin.h"
 #include "config.h"
 #include "wt-status.h"
+#include "commit.h"
+#include "pretty.h"
 
 int cmd_psuh(int argc, const char **argv, const char *prefix)
 {
@@ -8,6 +10,8 @@ int cmd_psuh(int argc, const char **argv, const char *prefix)
     int i;
     const char *cfg_name;
     struct wt_status status;
+    struct commit *c = NULL;
+    struct strbuf commitline = STRBUF_INIT;
     
     printf(_("Pedro sopla una hoja.\n"));
 
@@ -31,6 +35,13 @@ int cmd_psuh(int argc, const char **argv, const char *prefix)
     wt_status_prepare(the_repository, &status);
     git_config(git_default_config, &status);
     printf(_("Your current branch: %s\n"), status.branch);
+    
+    c = lookup_commit_reference_by_name("origin/master");
+
+    if (c != NULL) {
+        pp_commit_easy(CMIT_FMT_ONELINE, c, &commitline);
+        printf(_("Current commit: %s\n"), commitline.buf);
+    }
     
     return 0;
 }
