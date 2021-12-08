1eb05ab7f83e4ac7d1d96b91a68262157d7cc9a3
Johannes Schindelin <johannes.schindelin@gmx.de>
2021-12-08 15:16:24 +0100

    git-compat-util: untangle vendor-specific FLEX_ARRAY exceptions
    As suggested by Junio C Hamano, in preparation for adding yet another
    vendor-specific exception, let's break up the condition (which already
    made the line longer than 80 columns) into a vendor-specific part and a
    vendor-agnostic part.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- git-compat-util.h
+++ git-compat-util.h
@@ -46,14 +46,20 @@
 /*
  * See if our compiler is known to support flexible array members.
  */
-#if defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 199901L) && (!defined(__SUNPRO_C) || (__SUNPRO_C > 0x580))
-# define FLEX_ARRAY /* empty */
+#if defined(__SUNPRO_C) && (__SUNPRO_C <= 0x580)
+/*
+ * According to 203ee91fd273 (git-compat-util.h: avoid using c99 flex array
+ * feature with Sun compiler 5.8, 2009-06-08), this version cannot handle
+ * flexible array members.
+ */
 #elif defined(__GNUC__)
 # if (__GNUC__ >= 3)
 #  define FLEX_ARRAY /* empty */
 # else
 #  define FLEX_ARRAY 0 /* older GNU extension */
 # endif
+#elif defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 199901L)
+# define FLEX_ARRAY /* empty */
 #endif
 
 /*
