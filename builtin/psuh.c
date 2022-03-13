ff4ef91ef0860bf4f3831d03b7527b98500f11f4
Kartik <kartikprajapati987@gmail.com>
2022-03-13 23:03:33 +0530

    psuh: print the current branch
    Signed-off-by: Kartik <kartikprajapati987@gmail.com>

--- builtin/psuh.c
+++ builtin/psuh.c
@@ -2,11 +2,14 @@
 #include "gettext.h"
 #include <stdio.h>
 #include "config.h"
+#include "repository.h"
+#include "wt-status.h"
 
 int cmd_psuh(int argc, const char **argv, const char *prefix)
 {
     int i;
     const char *cfg_name;
+    struct wt_status status;
 
     printf(_("Pony saying hello goes here.\n"));
 
@@ -20,11 +23,13 @@ int cmd_psuh(int argc, const char **argv, const char *prefix)
     printf(_("Your current working directory:\n<top-leve>%s%s\n"),
             prefix ? "/" : "", prefix ? prefix : "");
 
-    git_config(git_default_config, NULL);
+    wt_status_prepare(the_repository, &status); 
+    git_config(git_default_config, &status);
     if (git_config_get_string_tmp("user.name", &cfg_name) > 0)
         printf(_("No name is found in config\n"));
     else
         printf(_("Your name: %s\n"), cfg_name);
 
+    printf(_("Your current branch: %s\n"), status.branch);
     return 0;
 }
