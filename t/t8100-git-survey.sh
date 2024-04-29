6259844052f34ef6c990c5b124cb7005d36f38f1
Jeff Hostetler <jeffhostetler@github.com>
2024-04-29 09:51:34 -0400

    survey: add command line opts to select references
    By default we will scan all references in "refs/heads/", "refs/tags/"
    and "refs/remotes/".
    
    Add command line opts let the use ask for all refs or a subset of them
    and to include a detached HEAD.
    
    Signed-off-by: Jeff Hostetler <git@jeffhostetler.com>
    Signed-off-by: Derrick Stolee <stolee@gmail.com>

--- t/t8100-git-survey.sh
+++ t/t8100-git-survey.sh
@@ -10,9 +10,13 @@ export TEST_PASSES_SANITIZE_LEAK
 
 . ./test-lib.sh
 
-test_expect_success 'git survey -h shows experimental warning' '
-	test_expect_code 129 git survey -h 2>usage &&
-	grep "EXPERIMENTAL!" usage
+test_expect_success 'create a semi-interesting repo' '
+	test_commit_bulk 10
+'
+
+test_expect_success 'git survey (default)' '
+	git survey >out 2>err &&
+	test_line_count = 0 err
 '
 
 test_done
