84362406b79febb97eed7038d280ec67c43bd354
Daniel Ploch <dploch@google.com>
2024-09-10 10:59:10 -0400

    psuh: show parameters & config opts
    Signed-off-by: Daniel Ploch <dploch@google.com>

--- builtin/psuh.c
+++ builtin/psuh.c
@@ -1,11 +1,26 @@
 #include "builtin.h"
+#include "config.h"
 #include "gettext.h"
 
 int cmd_psuh(int argc, const char **argv, const char *prefix)
 {
-    (void)argc;
-    (void)argv;
-    (void)prefix;
+    printf(Q_("Your args (there is %d):\n",
+	      "Your args (there are %d):\n",
+	      argc),
+	   argc);
+    for (int i = 0; i < argc; i++)
+	printf("%d: %s\n", i, argv[i]);
+
+    printf(_("Your current working directory:\n<top-level>%s%s\n"),
+	   prefix ? "/" : "", prefix ? prefix : "");
+
+    const char *cfg_name;
+    git_config(git_default_config, NULL);
+    if (git_config_get_string_tmp("user.name", &cfg_name) > 0)
+	printf(_("No name found in config\n"));
+    else
+	printf(_("Your name: %s\n"), cfg_name);
+
     printf(_("Pony saying hello goes here.\n"));
     return 0;
 }
