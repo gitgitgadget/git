b56f40e42efa025b45ae3f0240e5a78321e28504
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2024-10-04 12:12:24 +0100

    psuh: show parameters & config opts
    Signed-off-by: Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>

--- builtin/psuh.c
+++ builtin/psuh.c
@@ -1,10 +1,34 @@
+#define USE_THE_REPOSITORY_VARIABLE
+
 #include <stdio.h>
 #include "builtin.h"
+#include "config.h"
 #include "gettext.h"
+#include "repository.h"
+
+
+
 
 int cmd_psuh(int argc, const char **argv, const char *prefix, struct repository *repo) {
-    printf("%d\n, %s\n, %s\n", argc, argv[1], prefix);
+    const char *cfg_name;
+    int i;
+
     printf("%d\n", repo->different_commondir);
-    printf(_("Pony saying hello goes here.\n"));
+    printf(_("%s\n"), prefix);
+
+    printf(Q_("Your args (there is %d):\n",
+              "Your args (there are %d):\n",
+              argc),
+              argc);
+    for (i = 0; i < argc; i++)
+            printf("%d: %s\n", i, argv[i]);
+
+    printf(_("Your current working directory:\n<top-level>%s%s\n"),
+            prefix ? "/" : "", prefix ? prefix : "");
+    git_config(git_default_config, NULL);
+    if(git_config_get_string_tmp("user.name", &cfg_name) > 0)
+            printf(_("No name is found in config\n"));
+    else
+            printf(_("Your name: %s\n"), cfg_name);
     return 0;
 }
