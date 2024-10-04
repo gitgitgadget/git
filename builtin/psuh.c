26d948339a3fc1e6099a2d1d56fdc7839ac61194
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2024-10-04 18:45:12 +0100

    psuh: print the current branch
    Signed-off-by: Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>

--- builtin/psuh.c
+++ builtin/psuh.c
@@ -5,30 +5,38 @@
 #include "config.h"
 #include "gettext.h"
 #include "repository.h"
-
-
+#include "wt-status.h"
 
 
 int cmd_psuh(int argc, const char **argv, const char *prefix, struct repository *repo) {
-    const char *cfg_name;
-    int i;
+        const char *cfg_name;
+        struct wt_status status;
+        int i;
 
-    printf("%d\n", repo->different_commondir);
-    printf(_("%s\n"), prefix);
+        printf("%d\n", repo->different_commondir);
+        printf(_("%s\n"), prefix);
 
-    printf(Q_("Your args (there is %d):\n",
-              "Your args (there are %d):\n",
-              argc),
-              argc);
-    for (i = 0; i < argc; i++)
-            printf("%d: %s\n", i, argv[i]);
+        printf(Q_("Your args (there is %d):\n",
+                "Your args (there are %d):\n",
+                argc),
+                argc);
+        for (i = 0; i < argc; i++)
+                printf("%d: %s\n", i, argv[i]);
 
-    printf(_("Your current working directory:\n<top-level>%s%s\n"),
-            prefix ? "/" : "", prefix ? prefix : "");
-    git_config(git_default_config, NULL);
-    if(git_config_get_string_tmp("user.name", &cfg_name) > 0)
-            printf(_("No name is found in config\n"));
-    else
-            printf(_("Your name: %s\n"), cfg_name);
-    return 0;
+        printf(_("Your current working directory:\n<top-level>%s%s\n"),
+                prefix ? "/" : "", prefix ? prefix : "");
+
+        // git config implementation
+        git_config(git_default_config, NULL);
+        if(git_config_get_string_tmp("user.name", &cfg_name) > 0)
+                printf(_("No name is found in config\n"));
+        else
+                printf(_("Your name: %s\n"), cfg_name);
+
+        // getting current branch from status struct
+        wt_status_prepare(the_repository, &status);
+        git_config(git_default_config, &status);
+        printf(_("Your current branch: %s\n"), status.branch);
+        return 0;
+
 }
