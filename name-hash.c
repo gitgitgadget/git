d12ebc612c2b4d321648e28212cc213979ca6cd6
Alex Mironov <alexandrfox@gmail.com>
2025-05-21 11:49:10 +0200

    name-hash: don't add sparse directories in threaded lazy init
    Similarly to 5f116695864788d1fe45ff06bfad7a71a8d98d0a
    make sure to avoid placing sparse directories into the name_hash
    hashtable whenever multithreaded initialization is performed.
    
    Sparse directory entries represent a directory that is outside the
    sparse-checkout definition. These are not paths to blobs, so should not
    be added to the name_hash table as they must never be queried.
    
    Signed-off-by: Alex Mironov <alexandrfox@gmail.com>

--- name-hash.c
+++ name-hash.c
@@ -492,6 +492,9 @@ static void *lazy_name_thread_proc(void *_data)
 	for (k = 0; k < d->istate->cache_nr; k++) {
 		struct cache_entry *ce_k = d->istate->cache[k];
 		ce_k->ce_flags |= CE_HASHED;
+		if (S_ISSPARSEDIR(ce_k->ce_mode)) {
+			continue;
+		}
 		hashmap_entry_init(&ce_k->ent, d->lazy_entries[k].hash_name);
 		hashmap_add(&d->istate->name_hash, &ce_k->ent);
 	}
