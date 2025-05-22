8c209c6d45f494b6b2515cca2a12aced0de23826
Delilah Ashley Wu <delilahwu@microsoft.com>
2025-05-22 17:57:17 +1000

    lilah: Add nix flakes for my hot girl git-scm workspace
    Intentional --no-signoff; this commit should not be included in the
    final patch series.

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      with pkgs;
+      {
+        devShell = mkShell {
+          buildInputs = [
+            # Build tools
+            autoconf
+            clang-tools_19
+            clang_19
+            cmake
+            ninja
+            perl
+            lldb_19
+
+            # Deps
+            curl
+            zlib
+            expat
+          ];
+        };
+      }
+    );
+}
