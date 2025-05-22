ace72c40dabfd3415d379d573a894e4d1e7ca31f
Delilah Ashley Wu <delilahwu@microsoft.com>
2025-05-22 17:32:50 +1000

    config: test home and xdg files in `list --global`
    Checks for regressions in two existing behaviours of `git config list`,as described in the previous commit message:
        A. Without `--global`, it should not bail on unreadable/non-existent
           global config files.
        B. With `--global`, it should bail when both `$HOME/.gitconfig` and
           `$XDG_CONFIG_HOME/git/config` are unreadable. It should not bail
           when one or more of them is readable.
    
    Also adds a test for the issue that this contribution solves.
        C. `git config list --global` should include contents from both
           `$HOME/.gitconfig` and `$XDG_CONFIG_HOME/git/config` (assuming
           they are readable), not just the former.
    
    Signed-off-by: Delilah Ashley Wu <delilahwu@microsoft.com>

--- t/t1300-config.sh
+++ t/t1300-config.sh
@@ -2367,6 +2367,76 @@ test_expect_success '--show-scope with --default' '
 	test_cmp expect actual
 '
 
+test_expect_success 'list with nonexistent global config' '
+	rm -rf "$HOME"/.config/git/config &&
+	rm -f .git/config &&
+	git config ${mode_prefix}list --show-scope
+'
+
+test_expect_success 'list --global with nonexistent global config' '
+	rm -rf "$HOME"/.config/git/config &&
+	rm -f .git/config &&
+	test_must_fail git config ${mode_prefix}list --global --show-scope
+'
+
+test_expect_success 'list --global with only home' '
+	rm -rf "$HOME"/.config/git/config &&
+	rm -f .git/config &&
+
+	test_when_finished rm -f \"\$HOME\"/.gitconfig &&
+	cat >"$HOME"/.gitconfig <<-EOF &&
+	[home]
+		config = true
+	EOF
+
+	cat >expect <<-EOF &&
+	global	home.config=true
+	EOF
+	git config ${mode_prefix}list --global --show-scope >output &&
+	test_cmp expect output
+'
+
+test_expect_success 'list --global with only xdg' '
+	rm -f "$HOME"/.gitconfig .git/config &&
+
+	test_when_finished rm -rf \"\$HOME\"/.config/git &&
+	mkdir -p "$HOME"/.config/git &&
+	cat >"$HOME"/.config/git/config <<-EOF &&
+	[xdg]
+		config = true
+	EOF
+
+	cat >expect <<-EOF &&
+	global	xdg.config=true
+	EOF
+	git config ${mode_prefix}list --global --show-scope >output &&
+	test_cmp expect output
+'
+
+test_expect_success 'list --global with both home and xdg' '
+	rm -f .git/config &&
+
+	test_when_finished rm -f \"\$HOME\"/.gitconfig &&
+	cat >"$HOME"/.gitconfig <<-EOF &&
+	[home]
+		config = true
+	EOF
+
+	test_when_finished rm -rf \"\$HOME\"/.config/git &&
+	mkdir -p "$HOME"/.config/git &&
+	cat >"$HOME"/.config/git/config <<-EOF &&
+	[xdg]
+		config = true
+	EOF
+
+	cat >expect <<-EOF &&
+	global	xdg.config=true
+	global	home.config=true
+	EOF
+	git config ${mode_prefix}list --global --show-scope >output &&
+	test_cmp expect output
+'
+
 test_expect_success 'override global and system config' '
 	test_when_finished rm -f \"\$HOME\"/.gitconfig &&
 	cat >"$HOME"/.gitconfig <<-EOF &&
