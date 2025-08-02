423a778f389d2f2aab9fdf3729937b2465176da3
monarch <ayushoffinfo17@gmail.com>
2025-08-02 16:48:46 +0530

    psuh: print the current branch
    Signed-off-by: monarch <ayushoffinfo17@gmail.com>

--- builtin/psuh.c
+++ builtin/psuh.c
@@ -1,10 +1,16 @@
 #include "builtin.h"
 #include "gettext.h"
-#include <stdio.h>
 
-int cmd_psuh(int argc UNUSED, const char **argv UNUSED,
-	     const char *prefix UNUSED, struct repository *repo UNUSED)
+int cmd_psuh(int argc , const char **argv ,
+	     const char *prefix , struct repository *repo )
 {
-	printf(_("Pony saying hello goes here.\n"));
-	return 0;
+	int i;
+printf(Q_("Your args (there is %d):\n",
+	 "Your args (there are %d):\n",
+	 argc),
+       argc);
+for (i = 0; i < argc; i++)
+    printf("%d: %s\n", i, argv[i]);
+printf(_("Your current working directory: \n<top-level>%s%s\n"),
+       prefix ? "/" : "", prefix ? prefix : "");
 }
