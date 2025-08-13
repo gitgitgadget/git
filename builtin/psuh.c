54aeefa368d5e0a554c3063d8088521b7af99d32
monarch <ayushoffinfo17@gmail.com>
2025-08-14 01:14:38 +0530

    psuh: add usage text handling
    Implement usage text by integrating the parse-options API. This makes
    the command more robust by handling help flags like -h and --help
    gracefully instead of crashing.
    
    This also updates command-list.txt to make the 'psuh' command
    discoverable in the main 'git help' list.
    
    Signed-off-by: Your Name ayusharma1706@gmail.com

--- builtin/psuh.c
+++ builtin/psuh.c
@@ -6,6 +6,12 @@
 #include "commit.h"
 #include "pretty.h"
 #include "strbuf.h"
+#include "parse-options.h"
+
+static const char * const psuh_usage[] = {
+    N_("git psuh [<arg>...]"),
+    NULL,
+};
 
 int cmd_psuh(int argc, const char **argv,
 	     const char *prefix, struct repository *repo)
@@ -15,6 +21,12 @@ int cmd_psuh(int argc, const char **argv,
     struct wt_status status;
     struct commit *c = NULL;
     struct strbuf commitline = STRBUF_INIT;
+
+    struct option options[] = {
+	OPT_END()
+    };
+
+    argc = parse_options(argc, argv, prefix, options, psuh_usage, 0);
 
     printf(Q_("Your args (there is %d):\n",
 	      "Your args (there are %d):\n",
