30d2f5c05cf85ff2c6b99097772325a2964e561a
monarch <ayushoffinfo17@gmail.com>
2025-08-14 10:59:41 +0530

    psuh: add a simple test
    Add a test script for the new 'psuh' command.
    
    This test ensures the command runs without crashing and produces some
    expected output, which will help prevent future regressions. It also
    verifies that the command can correctly read the user.name
    configuration from the test's sandboxed environment.
    
    Signed-off-by: Your Name ayusharma1706@gmail.com

--- t/t9999-psuh-tutorial.sh
+++ t/t9999-psuh-tutorial.sh
@@ -1,19 +1,14 @@
 #!/bin/sh
 
 test_description='git-psuh test
-
 This test runs git-psuh and makes sure it does not crash.'
 
 . ./test-lib.sh
 
-test_expect_success 'git psuh prints a message' '
-	git psuh >actual &&
-	test_cmp /dev/null actual
-'
-
-test_expect_success 'git psuh does not crash' '
-	git psuh >actual 2>&1 &&
-	test $? -eq 0
+test_expect_success 'runs correctly with no args and good output' '
+    git config user.name "Test User" &&
+    git psuh >actual &&
+    grep "Your name:" actual
 '
 
 test_done
