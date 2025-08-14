d923d8b73cf6b9afd184bbfcaa2e61e5b2d8e9c0
monarch <ayushoffinfo17@gmail.com>
2025-08-14 14:50:17 +0530

    psuh: display the top of origin/master
    Use the lookup_commit_reference_by_name() and pp_commit_easy()
    helpers to find and print the summary of the commit at the
    tip of the upstream master branch.
    
    Signed-off-by: monarch <ayushshoffinfo17@gmail.com>

--- builtin/psuh.c
+++ builtin/psuh.c
@@ -14,7 +14,7 @@ static const char * const psuh_usage[] = {
 };
 
 int cmd_psuh(int argc, const char **argv,
-	     const char *prefix, struct repository *repo)
+             const char *prefix, struct repository *repo)
 {
     int i;
     const char *cfg_name;
@@ -23,34 +23,35 @@ int cmd_psuh(int argc, const char **argv,
     struct strbuf commitline = STRBUF_INIT;
 
     struct option options[] = {
-	OPT_END()
+        OPT_END()
     };
 
     argc = parse_options(argc, argv, prefix, options, psuh_usage, 0);
 
     printf(Q_("Your args (there is %d):\n",
-	      "Your args (there are %d):\n",
-	      argc),
-	   argc);
+              "Your args (there are %d):\n",
+              argc),
+           argc);
     for (i = 0; i < argc; i++)
-	printf("%d: %s\n", i, argv[i]);
+        printf("%d: %s\n", i, argv[i]);
     printf(_("Your current working directory: \n<top-level>%s%s\n"),
-	   prefix ? "/" : "", prefix ? prefix : "");
+           prefix ? "/" : "", prefix ? prefix : "");
 
-    repo_config(repo, git_default_config, NULL);
+    repo_config(repo, git_die_config, NULL);
+
     if (repo_config_get_string_tmp(repo, "user.name", &cfg_name))
-	printf(_("No name is found in config\n"));
+        printf(_("No name is found in config\n"));
     else
-	printf(_("Your name: %s\n"), cfg_name);
+        printf(_("Your name: %s\n"), cfg_name);
 
     wt_status_prepare(repo, &status);
-    repo_config(repo, git_default_config, &status);
+    repo_config(repo, git_die_config, &status);
     printf(_("Your current branch: %s\n"), status.branch);
 
     c = lookup_commit_reference_by_name("origin/master");
     if (c != NULL) {
-	pp_commit_easy(CMIT_FMT_ONELINE, c, &commitline);
-	printf(_("Current commit: %s\n"), commitline.buf);
+        pp_commit_easy(CMIT_FMT_ONELINE, c, &commitline);
+        printf(_("Current commit: %s\n"), commitline.buf);
     }
 
     strbuf_release(&commitline);
