97ba2e49b6a24f8c2383797f377c376f701ba406
Derrick Stolee <stolee@gmail.com>
2026-01-17 14:27:46 -0500

    config-batch: implement get v1
    The 'get' command for the 'git config-batch' builtin is the first command
    and is currently at version 1. It returns at most one value, the same as
    'git config --get <key>' with optional value-based filtering.
    
    The documentation and tests detail the specifics of how to format requests
    of this format and how to parse the results.
    
    Future versions could consider multi-valued responses or regex-based key
    matching.
    
    For the sake of incremental exploration of the potential in the 'git
    config-batch' command, this is the only implementation being presented in
    the first patch series.
    
    Future extensions could include a '-z' parameter that uses NUL bytes in the
    command and output format to allow for spaces or newlines in the input or
    newlines in the output.
    
    Signed-off-by: Derrick Stolee <stolee@gmail.com>

--- t/t1312-config-batch.sh
+++ t/t1312-config-batch.sh
@@ -16,10 +16,111 @@ test_expect_success 'unknown_command' '
 	test_cmp expect out
 '
 
+test_expect_success 'completely broken input' '
+	echo "not_even_two_tokens" >in &&
+	test_must_fail git config-batch 2>err <in &&
+	test_grep "expected at least 2 tokens" err &&
+	test_grep "an unrecoverable error occurred during command execution" err
+'
+
 test_expect_success 'failed to parse version' '
 	echo "bogus BAD_VERSION line of tokens" >in &&
 	test_must_fail git config-batch 2>err <in &&
 	test_grep BAD_VERSION err
+'
+
+test_expect_success 'get inherited config' '
+	test_when_finished git config --unset test.key &&
+
+	git config test.key "test value with spaces" &&
+
+	echo "get 1 inherited test.key" >in &&
+	echo "get 1 found test.key local test value with spaces" >expect &&
+	git config-batch >out <in &&
+	test_cmp expect out &&
+
+	echo "get 1 global test.key" >in &&
+	echo "get 1 missing test.key" >expect &&
+	git config-batch >out <in &&
+	test_cmp expect out
+'
+
+test_expect_success 'set up worktree' '
+	test_commit A &&
+	git config extensions.worktreeconfig true &&
+	git worktree add --detach worktree
+'
+
+test_expect_success 'get config with arg:regex' '
+	test_when_finished git config --unset-all test.key &&
+	GIT_CONFIG_SYSTEM=system-config-file &&
+	GIT_CONFIG_NOSYSTEM=0 &&
+	GIT_CONFIG_GLOBAL=global-config-file &&
+	export GIT_CONFIG_SYSTEM &&
+	export GIT_CONFIG_NOSYSTEM &&
+	export GIT_CONFIG_GLOBAL &&
+
+	git config --system test.key on1e &&
+	git config --global test.key t2wo &&
+	git config test.key "thre3e space" &&
+	git config --worktree test.key 4four &&
+
+	cat >in <<-\EOF &&
+	get 1 inherited test.key arg:regex .*1.*
+	get 1 inherited test.key arg:regex [a-z]2.*
+	get 1 inherited test.key arg:regex .*3e s.*
+	get 1 inherited test.key arg:regex 4.*
+	get 1 inherited test.key arg:regex .*5.*
+	get 1 inherited test.key arg:regex .*6.*
+	EOF
+
+	cat >expect <<-\EOF &&
+	get 1 found test.key system on1e
+	get 1 found test.key global t2wo
+	get 1 found test.key local thre3e space
+	get 1 found test.key worktree 4four
+	get 1 found test.key command five5
+	get 1 missing test.key .*6.*
+	EOF
+
+	git -c test.key=five5 config-batch >out <in &&
+	test_cmp expect out
+'
+
+test_expect_success 'get config with arg:fixed-value' '
+	test_when_finished git config --unset-all test.key &&
+	GIT_CONFIG_SYSTEM=system-config-file &&
+	GIT_CONFIG_NOSYSTEM=0 &&
+	GIT_CONFIG_GLOBAL=global-config-file &&
+	export GIT_CONFIG_SYSTEM &&
+	export GIT_CONFIG_NOSYSTEM &&
+	export GIT_CONFIG_GLOBAL &&
+
+	git config --system test.key one &&
+	git config --global test.key two &&
+	git config test.key "three space" &&
+	git config --worktree test.key four &&
+
+	cat >in <<-\EOF &&
+	get 1 inherited test.key arg:fixed-value one
+	get 1 inherited test.key arg:fixed-value two
+	get 1 inherited test.key arg:fixed-value three space
+	get 1 inherited test.key arg:fixed-value four
+	get 1 inherited test.key arg:fixed-value five
+	get 1 inherited test.key arg:fixed-value six
+	EOF
+
+	cat >expect <<-\EOF &&
+	get 1 found test.key system one
+	get 1 found test.key global two
+	get 1 found test.key local three space
+	get 1 found test.key worktree four
+	get 1 found test.key command five
+	get 1 missing test.key six
+	EOF
+
+	git -c test.key=five config-batch >out <in &&
+	test_cmp expect out
 '
 
 test_done
