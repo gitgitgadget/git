dc49bd8d87975ceb13f7172bf5daa623b92f7cbb
Long Nguyen <n.h.long.9697@gmail.com>
2026-01-17 15:55:00 +0700

    psuh: add a built-in by popular demand
    Internal metrics indicate this is a command many users expect to be
    present. So here's an implementation to help drive customer
    satisfaction and engagement: a pony which doubtfully greets the user,
    or, a Pony Saying "Um, Hello" (PSUH).
    
    This commit message is intentionally formatted to 72 columns per line,
    starts with a single line as "commit message subject" that is written as
    if to command the codebase to do something (add this, teach a command
    that). The body of the message is designed to add information about the
    commit that is not readily deduced from reading the associated diff,
    such as answering the question "why?".
    
    Signed-off-by: Long Nguyen <n.h.long.9697@gmail.com>

---
+++ builtin/psuh.c
@@ -0,0 +1,8 @@
+#include "builtin.h"
+#include "gettext.h"
+
+int cmd_psuh(int argc UNUSED, const char **argv UNUSED,
+	const char *prefix UNUSED, struct repository *repo UNUSED) {
+    printf(_("Pony saying hello goes here.\n"));
+    return 0;
+}
