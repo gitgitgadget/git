05a421ce81fb1e5646f21b162fb06c06f613dba7
Derrick Stolee <stolee@gmail.com>
2026-01-21 21:31:52 -0500

    config-batch: add NUL-terminated I/O format
    When using automated tools, it is critical to allow for input/output formats
    that include special characters such as spaces and newlines. While the
    existing protocol for 'git config-batch' is human-readable and has some
    capacity for some spaces in certain positions, it is not available for
    spaces in the config key or newlines in the config values.
    
    Add the '-z' option to signal the use of NUL-terminated strings. To
    understand where commands end regardless of potential future formats, use
    two NUL bytes in a row to terminate a command. To allow for empty string
    values, each token is provided in a <length>:<value> format, making "0:"
    the empty string value.
    
    Update the existing 'help' and 'get' commands to match this format. Create
    helper methods that make it easy to parse and print in both formats
    simultaneously.
    
    Signed-off-by: Derrick Stolee <stolee@gmail.com>

--- t/t1312-config-batch.sh
+++ t/t1312-config-batch.sh
@@ -4,6 +4,26 @@ test_description='Test git config-batch'
 
 . ./test-lib.sh
 
+# usage: test_zformat <command> <args> <in >out
+#
+# Let 'in' be a z-format input but with " NUL " between tokens in
+# a single command and " NUL NUL" trailing each line.
+#
+# The values in 'out' will be space- and newline-delimited where
+# NUL-bytes would normally be output.
+test_zformat () {
+	sed -e "s/\ NUL\ /!/g" >nullin1 &&
+	sed -e "s/NUL//g" <nullin1 >nullin2 &&
+
+	tr "!" "\0" <nullin2 >nullin3 &&
+	tr "\n" "\0" <nullin3 >zin &&
+
+	$* <zin >zout &&
+
+	tr "\0" " " <zout >outspace &&
+	sed "s/\ \ /\n/g" <outspace
+}
+
 test_expect_success 'no commands' '
 	echo | git config-batch >out &&
 	test_must_be_empty out
@@ -33,6 +53,23 @@ test_expect_success 'help command' '
 	EOF
 
 	git config-batch >out <in &&
+	test_cmp expect out
+'
+
+test_expect_success 'help -z' '
+	cat >in <<-\EOF &&
+	4:help NUL 1:1 NUL NUL
+	5:bogus NUL 2:10 NUL NUL
+	EOF
+
+	cat >expect <<-\EOF &&
+	4:help 1:1 5:count 1:2
+	4:help 1:1 4:help 1:1
+	4:help 1:1 3:get 1:1
+	15:unknown_command
+	EOF
+
+	test_zformat git config-batch -z >out <in &&
 	test_cmp expect out
 '
 
@@ -133,6 +170,38 @@ test_expect_success 'get config with arg:fixed-value' '
 	EOF
 
 	git -c test.key=five config-batch >out <in &&
+	test_cmp expect out
+'
+
+test_expect_success 'get config with -z' '
+	test_when_finished git config --unset-all test.key &&
+	GIT_CONFIG_SYSTEM=system-config-file &&
+	GIT_CONFIG_NOSYSTEM=0 &&
+	GIT_CONFIG_GLOBAL=global-config-file &&
+	export GIT_CONFIG_SYSTEM &&
+	export GIT_CONFIG_NOSYSTEM &&
+	export GIT_CONFIG_GLOBAL &&
+
+	git config --system test.key on1e &&
+	git config --global test.key t2wo &&
+	git config test.key "thre3e space" &&
+	git config --worktree test.key 4four &&
+
+	cat >in <<-\EOF &&
+	3:get NUL 1:1 NUL 9:inherited NUL 8:test.key NUL NUL
+	3:get NUL 1:1 NUL 6:global NUL 8:test.key NUL 9:arg:regex NUL 3:2.* NUL NUL
+	3:get NUL 1:1 NUL 5:local NUL 8:test.key NUL 15:arg:fixed-value NUL 12:thre3e space NUL NUL
+	3:get NUL 1:1 NUL 9:inherited NUL 11:key.missing NUL NUL
+	EOF
+
+	cat >expect <<-\EOF &&
+	3:get 1:1 5:found 8:test.key 8:worktree 5:4four
+	3:get 1:1 5:found 8:test.key 6:global 4:t2wo
+	3:get 1:1 5:found 8:test.key 5:local 12:thre3e space
+	3:get 1:1 7:missing 11:key.missing
+	EOF
+
+	test_zformat git config-batch -z >out <in &&
 	test_cmp expect out
 '
 
