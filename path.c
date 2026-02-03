f8f2c613260458e3c86109888ee1c51313095319
Derrick Stolee <stolee@gmail.com>
2026-02-03 18:01:07 -0500

    backfill: work with prefix pathspecs
    The previous change allowed specifying revision arguments over the 'git
    backfill' command-line. This created the opportunity for restricting the
    initial commit set by filtering the revision walk through a pathspec. Other
    than filtering the commit set (and thereby the root trees), this did not
    restrict the path-walk implementation of 'git backfill' and did not restrict
    the blobs that were downloaded to only those matching the pathspec.
    
    Update the path-walk API to accept certain kinds of pathspecs and to
    silently ignore anything too complex, for now. We will update this in the
    next change to properly restrict to even complex pathspecs.
    
    The current behavior focuses on pathspecs that match paths exactly. This
    includes exact filenames, including directory names as prefixes. Pathspecs
    containing wildcards or magic are cleared so the path walk downloads all
    blobs, as before.
    
    The reason for this restriction is to allow for a faster execution by
    pruning the path walk to only trees that could contribute towards one of
    those paths as a parent directory.
    
    The test directory 'd/f/' (next to 'd/file*.txt') was prepared in a
    previous commit to exercise the subtlety in prefix matching.
    
    Signed-off-by: Derrick Stolee <stolee@gmail.com>

--- path.c
+++ path.c
@@ -57,7 +57,7 @@ static void strbuf_cleanup_path(struct strbuf *sb)
 		strbuf_remove(sb, 0, path - sb->buf);
 }
 
-static int dir_prefix(const char *buf, const char *dir)
+int dir_prefix(const char *buf, const char *dir)
 {
 	int len = strlen(dir);
 	return !strncmp(buf, dir, len) &&
