dd333093442dc2b9ffafc761d6ca23f94fe9ec1f
Harald Nordgren <haraldnordgren@gmail.com>
2026-05-12 18:39:34 +0200

    branch: add branch.<name>.pruneMerged opt-out
    Setting branch.<name>.pruneMerged=false exempts that branch from
    --prune-merged, even with --force. Useful for keeping a topic
    branch around between rounds.
    
    Explicit deletion via 'git branch -d' is unaffected.
    
    Signed-off-by: Harald Nordgren <haraldnordgren@gmail.com>

--- t/t3200-branch.sh
+++ t/t3200-branch.sh
@@ -1867,4 +1867,45 @@ test_expect_success '--prune-merged protects only the default branch by name, no
 	test_must_fail git -C pm-default-alias rev-parse --verify refs/heads/trunk
 '
 
+test_expect_success '--prune-merged honours branch.<name>.pruneMerged=false' '
+	test_when_finished "rm -rf pm-optout" &&
+	git clone pm-upstream pm-optout &&
+	git -C pm-optout branch one one-commit &&
+	git -C pm-optout branch --set-upstream-to=origin/next one &&
+	git -C pm-optout branch two two-commit &&
+	git -C pm-optout branch --set-upstream-to=origin/next two &&
+	git -C pm-optout config branch.one.pruneMerged false &&
+
+	git -C pm-optout branch --prune-merged origin 2>err &&
+
+	git -C pm-optout rev-parse --verify refs/heads/one &&
+	test_must_fail git -C pm-optout rev-parse --verify refs/heads/two &&
+	test_grep "Skipping .one." err
+'
+
+test_expect_success '--prune-merged --force still honours pruneMerged=false' '
+	test_when_finished "rm -rf pm-optout-force" &&
+	git clone pm-upstream pm-optout-force &&
+	git -C pm-optout-force checkout -b wip origin/wip &&
+	git -C pm-optout-force branch --set-upstream-to=origin/next wip &&
+	test_commit -C pm-optout-force local-only &&
+	git -C pm-optout-force checkout - &&
+	git -C pm-optout-force config branch.wip.pruneMerged false &&
+
+	git -C pm-optout-force branch --force --prune-merged origin &&
+
+	git -C pm-optout-force rev-parse --verify refs/heads/wip
+'
+
+test_expect_success 'branch -d still deletes a pruneMerged=false branch' '
+	test_when_finished "rm -rf pm-optout-d" &&
+	git clone pm-upstream pm-optout-d &&
+	git -C pm-optout-d branch one one-commit &&
+	git -C pm-optout-d branch --set-upstream-to=origin/next one &&
+	git -C pm-optout-d config branch.one.pruneMerged false &&
+
+	git -C pm-optout-d branch -d one &&
+	test_must_fail git -C pm-optout-d rev-parse --verify refs/heads/one
+'
+
 test_done
